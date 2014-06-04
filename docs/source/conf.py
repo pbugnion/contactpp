124f6212a42ee4290e6913223a022da1644dbae5
Pascal Bugnion <pob24@cam.ac.uk>
2014-06-04 11:05:11 +0100

    BLD: Test removing readthedocs hack.
    If contactpp can access RTD site-packages, and these include scipy, the hack should be unnecessary.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -20,33 +20,6 @@ import os
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #sys.path.insert(0, os.path.abspath('.'))
 
-# HACK FOR Read The Docs
-# Need to mock scipy.
-# See http://read-the-docs.readthedocs.org/en/latest/faq.html#i-get-import-errors-on-libraries-that-depend-on-c-modules
-class Mock(object):
-
-    __all__ = []
-
-    def __init__(self, *args, **kwargs):
-        pass
-
-    def __call__(self, *args, **kwargs):
-        return Mock()
-
-    @classmethod
-    def __getattr__(cls, name):
-        if name in ('__file__', '__path__'):
-            return '/dev/null'
-        elif name[0] == name[0].upper():
-            mockType = type(name, (), {})
-            mockType.__module__ = __name__
-            return mockType
-        else:
-            return Mock()
-
-MOCK_MODULES = ['scipy']
-for mod_name in MOCK_MODULES:
-    sys.modules[mod_name] = Mock()
 
 # -- General configuration ------------------------------------------------
 
