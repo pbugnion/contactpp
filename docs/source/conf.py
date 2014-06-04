656eb4df6da974555b561ba915bae577eb7d565e
Pascal Bugnion <pob24@cam.ac.uk>
2014-06-04 10:43:07 +0100

    BLD: Updated hack in conf.py.
    Removed dependency on mock.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -21,13 +21,32 @@ import os
 #sys.path.insert(0, os.path.abspath('.'))
 
 # HACK FOR Read The Docs
-# See http://blog.rtwilson.com/how-to-make-your-sphinx-documentation-compile-with-readthedocs-when-youre-using-numpy-and-scipy/
-# for details.
-import mock
- 
-MOCK_MODULES = [ 'scipy', 'scipy.optimize', 'scipy.integrate', 'scipy.special' ]
+# Need to mock scipy.
+# See http://read-the-docs.readthedocs.org/en/latest/faq.html#i-get-import-errors-on-libraries-that-depend-on-c-modules
+class Mock(object):
+
+    __all__ = []
+
+    def __init__(self, *args, **kwargs):
+        pass
+
+    def __call__(self, *args, **kwargs):
+        return Mock()
+
+    @classmethod
+    def __getattr__(cls, name):
+        if name in ('__file__', '__path__'):
+            return '/dev/null'
+        elif name[0] == name[0].upper():
+            mockType = type(name, (), {})
+            mockType.__module__ = __name__
+            return mockType
+        else:
+            return Mock()
+
+MOCK_MODULES = ['scipy']
 for mod_name in MOCK_MODULES:
-    sys.modules[mod_name] = mock.Mock()
+    sys.modules[mod_name] = Mock()
 
 # -- General configuration ------------------------------------------------
 
